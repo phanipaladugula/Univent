427e00e15fc9832b0492280ec645a97eb38e63dc
Phani <phanipaladugulaa@gmail.com>
2026-04-04 22:36:55 +0530

    feat: initialize Spring Boot backend with MCSR structure

---
+++ Backend/Springboot/Univent-Backend/src/test/java/Univent/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Univent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
