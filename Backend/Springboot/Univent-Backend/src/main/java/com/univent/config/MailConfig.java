1a6c553b23372f68d0bfa2879297cd1f1ac8a41e
Phani <phanipaladugulaa@gmail.com>
2026-04-18 19:17:51 +0530

    fix: logic improved

--- Backend/Springboot/Univent-Backend/src/main/java/com/univent/config/MailConfig.java
+++ Backend/Springboot/Univent-Backend/src/main/java/com/univent/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.univent.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,25 +8,27 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+
+
 @Configuration
 public class MailConfig {
+    @Value("${spring.mail.host}") private String host;
+    @Value("${spring.mail.port}") private int port;
+    @Value("${spring.mail.username}") private String username;
+    @Value("${spring.mail.password}") private String password;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("admin.univent@gmail.com");
-        mailSender.setPassword("ycnktsfqkcmjisjt"); // Your Gmail app password
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.debug", "true");
-        props.put("mail.from", "admin.univent@gmail.com"); // Critical property
-
         return mailSender;
     }
 }
